971fd7717696c374d8e58f9a63682a7d1c2bae1d
Sjon Hortensius <sjon@hortensius.net>
2017-02-19 12:34:46 +0100

    Transport/Curl - allow additional curl options to be passed

--- Transport/Curl.php
+++ Transport/Curl.php
@@ -4,10 +4,17 @@ use TooBasic\Exception;
 
 class Curl implements Rpc\Transport
 {
+	protected $_options = [];
+
 	public function __construct(Rpc\Transport $nextTransport = null)
 	{
 		if (isset($nextTransport))
 			throw new Exception(__CLASS__. ' does not support chaining');
+	}
+
+	public function setOption(int $key, $value)
+	{
+		$this->_options[ $key ] = $value;
 	}
 
 	public function request(string $method, string $url, array $headers = [], string $body = null): string
@@ -20,6 +27,9 @@ class Curl implements Rpc\Transport
 
 		try
 		{
+			// User-options first, so we can override them
+			curl_setopt_array($c, $this->_options);
+
 			curl_setopt($c, CURLOPT_CUSTOMREQUEST, $method);
 			curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
