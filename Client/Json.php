4dc9742e4215421ca6fe7abecd76b88eeababec6
Sjon Hortensius <sjon@hortensius.net>
2017-07-30 16:22:20 +0200

    Introduce setDebug() with output in _request

--- Client/Json.php
+++ Client/Json.php
@@ -8,6 +8,7 @@ class Json
 	protected $_transport;
 	protected static $_id = 1;
 	protected $_isNotification = false;
+	protected $_enableDebug = false;
 
 	public function __construct(string $url, Rpc\Transport $transport)
 	{
@@ -18,6 +19,11 @@ class Json
 	public function setNotification(bool $n = true): void
 	{
 		$this->_isNotification = $n;
+	}
+
+	public function setDebug(bool $d = true): void
+	{
+		$this->_enableDebug = $d;
 	}
 
 	public function __call(string $method, array $params)
@@ -46,6 +52,9 @@ class Json
 	{
 		$raw = $this->_transport->request('POST', $this->_url, ['Content-type' => 'application/json'], json_encode($data));
 		$response = json_decode($raw);
+
+		if ($this->_enableDebug)
+			print '<fieldset><legend>'.__CLASS__.'::'.__FUNCTION__.' debug output</legend><pre>'. print_r(['request'=>$data, 'response'=>$response], true). '</pre></fieldset>';
 
 		if (false == $response)
 			throw new Exception('Could not decode response as json: `%s`', [$raw]);
